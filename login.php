f786367457bc95c2ca8023c46fe46e543423d7d4
“WinmaH” <winma.15@cse.mrt.ac.lkcd>
2017-11-02 18:45:42 +0530

    login

---
+++ login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Login Form</title>
+
+    <link rel="stylesheet" href="css/demo.css">
+    <link rel="stylesheet" href="css/login.css">
+
+</head>
+
+
+
+
+
+
+<body>
+
+
+<header id="header">
+    <h1 style="text-align: center"><strong>CRESCENDO MUSIC ACADEMY </strong></h1>
+    <!--  <span class="avatar"><img src="images/avatar.jpg" alt="" /></span> -->
+</header>
+
+<!-- You only need this form and the form-login.css -->
+
+<form class="form-login" method="post" action="#">
+
+    <div class="form-log-in-with-email">
+
+        <div class="form-white-background">
+
+            <div class="form-title-row">
+                <h1>Log in</h1>
+            </div>
+
+            <div class="form-row">
+                <label>
+                    <span>User Name</span>
+                    <input type="email" name="email">
+                </label>
+            </div>
+
+            <div class="form-row">
+                <label>
+                    <span>Password</span>
+                    <input type="password" name="password">
+                </label>
+            </div>
+
+            <div class="form-row">
+                <button type="submit">Log in</button>
+            </div>
+
+        </div>
+
+
+
+    </div>
+
+</form>
+
+
+
+</body>
+
+</html>
