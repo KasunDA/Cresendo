7b3c935d6466e645a2805fd9e73d6014e6b9567a
Madushi <madushi.15@cse.mrt.ac.lk>
2017-11-04 20:49:46 +0530

    view class details(not completed)

--- connect.php
+++ connect.php
@@ -5,7 +5,6 @@ function connect()
     define('DB_NAME', 'db_group');
     define('DB_USER','root');
     define('DB_PASSWORD','');
-
     $con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysqli_error());
     $db=mysqli_select_db($con,DB_NAME) or die("Failed to connect to MySQL: " . mysqli_error());
     return $con;
