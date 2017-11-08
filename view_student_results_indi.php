2d6f78711c5e2902e42141b05566d52a912c07b4
Thakshila <thakshilasilva.15@cse.mrt.ac.lk>
2017-11-08 09:52:16 +0530

    view_results_teacher

--- view_student_results_indi.php
+++ view_student_results_indi.php
@@ -1,106 +1,78 @@
 <?php
-include 'connect.php';
-$con= connect();
-#session_start();
-#$_SESSION['id']=$id;
+include "connect.php";
+$con = connect();
 ?>
 
-<!DOCTYPE html>
-<html>
 
 <head>
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>View Student Results</title>
+    <title>View Results</title>
 
     <link rel="stylesheet" href="css/demo.css">
-    <link rel="stylesheet" href="css/view_results_choose.css">
 
+    <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css" type="text/css" />
+
+
+    <script type="text/javascript" src="http://code.jquery.com/jquery.js"></script>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-ui.js"></script>
+
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="http://code.jquery.com/ui/1.10.1/jquery-ui.min.js"></script>
+
+    <script type="text/javascript">
+        $(function() {
+
+            //autocomplete
+            $(".auto1").autocomplete({
+                source: "search_class.php",
+                minLength: 1
+            });
+
+        });
+    </script>
 </head>
-
-
 <header>
-    <h1 style="..."><strong>CRESCENDO MUSIC ACADEMY</strong></h1>
+    <h1>CRESCENDO MUSIC ACADEMY</h1>
+
 </header>
 
-
+<body>
 <div class="main-content">
 
 
-    <div class="form-mini-container">
+    <form class="form-basic"  method="post" action="view_results.php">
 
+        <div class="form-title-row">
+            <h1>View Results</h1>
+        </div>
 
-        <h1>View Student Results</h1>
+        <div class="form-row">
+            <label>
+                <span>Class :</span>
+                <input type="text" name="class1" class="auto1" required >
+            </label>
+        </div>
 
-        <form class="form-mini" method="post" action="#">
+        <div class="form-row">
+            <label>
+                <span>Exam Title:</span>
+                <select name="ETitle" value="ETitle" required>
+                    <option>Exam-1</option>
+                    <option>Exam-2</option>
+                    <option>Exam-Final</option>
+                </select>
+            </label>
+        </div>
 
-            <div class="form-row">
-                <input type="text" name="name" placeholder="Your Name">
-            </div>
+        <div class="form-row">
+            <button type="submit" name="submit">View Results</button>
+        </div>
 
-            <div class="form-row">
-                <input type="email" name="email" placeholder="Your Email">
-            </div>
-
-            <div class="form-row">
-                <label>
-                    <select name="dropdown">
-                        <option>Choose an option</option>
-                        <option>Option One</option>
-                        <option>Option Two</option>
-                        <option>Option Three</option>
-                        <option>Option Four</option>
-                    </select>
-                </label>
-            </div>
-
-            <div class="form-row">
-                <label class="form-checkbox">
-                    <input type="checkbox" name="checkbox" checked>
-                    <span>Checkbox</span>
-                </label>
-            </div>
-
-            <div class="form-row">
-                <div class="form-radio-buttons">
-
-                    <div>
-                        <label>
-                            <input type="radio" name="radio">
-                            <span>Radio option 1</span>
-                        </label>
-                    </div>
-
-                    <div>
-                        <label>
-                            <input type="radio" name="radio">
-                            <span>Radio option 2</span>
-                        </label>
-                    </div>
-
-                    <div>
-                        <label>
-                            <input type="radio" name="radio">
-                            <span>Radio option 3</span>
-                        </label>
-                    </div>
-
-                </div>
-            </div>
-
-            <div class="form-row form-last-row">
-                <button type="submit">Submit Form</button>
-            </div>
-
-        </form>
-    </div>
-
-
+    </form>
 </div>
 
 </body>
 
-</html>
